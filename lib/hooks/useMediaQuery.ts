9a3fa13614066e10eb964b25b752cb4113991cbb
Lorenzo Dessimoni <lorenzo.dessimoni@gmail.com>
2025-05-02 11:39:03 +0200

    Add getServerSnapshot to useMediaQuery hook

--- lib/hooks/useMediaQuery.ts
+++ lib/hooks/useMediaQuery.ts
@@ -1,21 +1,29 @@
 import React from "react";
 
-export function useMediaQuery(query: string) {
-    const subscribe = React.useCallback(
-        (callback: () => void) => {
-            const matchMedia = window.matchMedia(query);
+export function useMediaQuery(
+    query: string,
+    serverFallback?: boolean
+): boolean {
+    const getServerSnapshot = () =>
+        undefined !== serverFallback ? serverFallback : false;
 
-            matchMedia.addEventListener("change", callback);
-            return () => {
-                matchMedia.removeEventListener("change", callback);
-            };
-        },
-        [query]
+    const [getSnapshot, subscribe] = React.useMemo(() => {
+        const mediaQueryList = window.matchMedia(query);
+
+        return [
+            () => mediaQueryList.matches,
+            (notify: () => void) => {
+                mediaQueryList.addEventListener("change", notify);
+                return () => {
+                    mediaQueryList.removeEventListener("change", notify);
+                };
+            },
+        ];
+    }, [query]);
+
+    return React.useSyncExternalStore(
+        subscribe,
+        typeof window !== "undefined" ? getSnapshot : getServerSnapshot,
+        getServerSnapshot
     );
-
-    const getSnapshot = () => {
-        return window.matchMedia(query).matches;
-    };
-
-    return React.useSyncExternalStore(subscribe, getSnapshot);
 }
